0ea63e042859b407a6d64261004787e1f95217ac
David Goemans <dgoemans@gmail.com>
2015-07-27 22:32:32 +0200

    - Removed old code

--- src/Model.js
+++ src/Model.js
@@ -141,47 +141,6 @@ define(["Phaser", "Camera"],
             }, this);
 
             this.graphics.endFill();
-
-            return;
-            /*
-
-            OLD STUFF BELOW!!!!
-
-             */
-            this.graphics.clear();
-            //this.graphics.lineStyle(1, 0x0000FF, 1);
-
-            this.graphics.beginFill(0xFF0000, 1);
-
-            var pos = this.vector3to4(this.vertices[0]);
-            pos = this.matrix.multiply(pos);
-            pos = Camera.getInstance().matrix.multiply(pos);
-            pos = this.normalizeProjectedVector(pos);
-
-            this.graphics.moveTo(pos.elements[0], pos.elements[1]);
-            var lightPos = Vector.create([20, 20, 30]);
-            var length = lightPos.modulus() + 0;
-
-            for (var i = 1; i <= this.vertices.length; i++)
-            {
-                pos = this.vector3to4(this.vertices[i % this.vertices.length]);
-                pos = this.matrix.multiply(pos);
-                var dist = pos.distanceFrom(this.vector3to4(lightPos));
-                pos = Camera.getInstance().matrix.multiply(pos);
-                pos = this.normalizeProjectedVector(pos);
-
-                var color = this.baseColor.clone();
-                for (var j = 0; j < color.length; j++)
-                {
-                    color[j] = color[j] * 0.7 + color[j] * 0.3 * (1 - dist / length * dist / length);
-                }
-
-                var vColor = Phaser.Color.toRGB(color[0], color[1], color[2]);
-                this.graphics.beginFill(vColor, 1);
-                this.graphics.lineTo(pos.elements[0], pos.elements[1]);
-            }
-
-            this.graphics.endFill();
         };
 
         Model.prototype.update = function ()
